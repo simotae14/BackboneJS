afbc1badb59c87c288fa53bd3d874aacd47c1123
Simotae14 <startae14@gmail.com>
2018-01-16 00:05:59 +0100

    Creating Models

--- Boilerplate/js/main.js
+++ Boilerplate/js/main.js
@@ -2,3 +2,14 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
+
+/**************************************
+MODEL
+**************************************/
+var Song = Backbone.Model.extend({
+	initialize: function(){
+		console.log("Creata una nuova canzone.");
+	}
+});
+
+var song = new Song();
