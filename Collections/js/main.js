b6d6d142f7c663929ee698961db83d013fcf9086
Simone Taeggi <simone.taeggi@leovegas.com>
2018-01-17 18:11:27 +0100

    Creating Collections

--- Collections/js/main.js
+++ Collections/js/main.js
@@ -4,69 +4,21 @@
 // files and modules.
 
 /**************************************
-MODEL
+COLLECTION
 **************************************/
-/*
-var Song = Backbone.Model.extend({
-	initialize: function(){
-		console.log("Creata una nuova canzone.");
-	}
+
+var Song = Backbone.Model.extend();
+
+// DEFINISCO LA COLLECTION
+var Songs = Backbone.Collection.extend({
+	model: Song
 });
 
-var song = new Song();
-*/
-/**************************************
-ATTRIBUTI DEI MODEL
-**************************************/
-/*
-var Song = Backbone.Model.extend({
-	defaults: {
-		genere: "Jazz"
-	}
-});
+var songs = new Songs([
+	new Song({ title: "Song 1" }),
+	new Song({ title: "Song 2" }),
+	new Song({ title: "Song 3" })
+]);
 
-var song = new Song({
-	titolo: "Blue in Green",
-	artista: "Miles Davis",
-	annoPubblicazione: 1959	
-});
-*/
-/**************************************
-VALIDAZIONE DEI MODEL
-**************************************/
-/*
-var Song = Backbone.Model.extend({
-	validate: function(attrs){
-		if(!attrs.titolo) 
-			return "Il Titolo è richiesto";
-	}
-});
-
-var song = new Song();
-
-console.log(song.isValid());
-
-console.log(song.validationError);
-*/
-/**************************************
-EREDITARIETA' DEI MODEL
-**************************************/
-var Animale = Backbone.Model.extend({
-	cammina: function(){
-		console.log("Animale che cammina...");
-	}
-});
-
-// figlio
-var Cane = Animale.extend({
-	cammina: function(){
-		// uso funzione ereditata
-		Animale.prototype. cammina.apply(this);
-
-		console.log("Cane che cammina...");
-	}
-});
-
-var cane = new Cane();
-
-cane.cammina();
+// creo una istanza di Model dentro la collection
+songs.add(new Song({ title: "Song 4" }));
