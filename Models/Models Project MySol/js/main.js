ab18d04cdff6956ebb96abbef63a85a227ee051f
Simone Taeggi <simone.taeggi@leovegas.com>
2018-01-17 16:54:58 +0100

    Models Project

--- Models/Models Project MySol/js/main.js
+++ Models/Models Project MySol/js/main.js
@@ -10,16 +10,42 @@ var Veicolo = Backbone.Model.extend({
 
 	
 	validate: function(attrs) {
-		if(attrs.numeroRegistrazione === null || attrs.numeroRegistrazione === undefined){
+		if(attrs.numeroRegistrazione){
 			return "Il numero di Registrazione deve essere valorizzato";
 		}
 	},
 
 	start: function() {
-		console.log("L'auto con numero di registrazione " + this.numeroRegistrazione + " è partita.");
+		console.log("Veicolo inizializzato.");
 	}
 
 });
 
 
+var Auto = Veicolo.extend({
+	start: function() {
+		console.log("L'auto con numero di registrazione " + this.get("numeroRegistrazione") + " è partita.");
+	}
+});
 
+
+var newAuto = new Auto({
+	numeroRegistrazione: 'XLI887',
+	colore: "Blue"
+});
+
+// cancellare valore del numeroRegistrazione dall'istanza Auto
+newAuto.unset("numeroRegistrazione");
+
+// controllo se è valido
+if(!newAuto.isValid()){
+	console.log(newAuto.validationError);
+}
+
+newAuto.set("numeroRegistrazione", "XLI887");
+
+if(!newAuto.isValid()){
+	console.log(newAuto.validationError);
+}
+
+newAuto.start();
